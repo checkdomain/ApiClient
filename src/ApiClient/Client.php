61c1b98c98474d311888f9a45ec7769b534d59f3
Julian Bente <jbente@Julians-MacBook-Air.local>
2017-12-20 10:48:10 +0100

    Change: spaghetti

--- src/ApiClient/Client.php
+++ src/ApiClient/Client.php
@@ -2,32 +2,74 @@
 
 namespace ApiClient;
 
-use ApiClient\Client\Core;
-use ApiClient\Client\Articles;
-use ApiClient\Client\Domains;
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Exception\RequestException;
 
+/**
+ * Guzzle Client Wrapper für die Checkdomain API
+ *
+ */
 class Client
 {
+    const BASE_URI = "";
+
+    /**
+     * @var GuzzleClient
+     */
+    protected $guzzleClient;
+
     /**
      * @var array
      */
-    private $config;
+    protected $guzzleOptions;
 
-    public function __construct($baseUri, $accessToken)
+    public function __construct($token)
     {
-        $this->config = [
-            'base_uri' => $baseUri,
-            'token' => $accessToken
+        $this->buildHeader($token);
+
+        $this->guzzleClient = new GuzzleClient([
+            'base_uri' => self::BASE_URI,
+        ]);
+    }
+
+    public function request(
+        $methode,
+        $uri,
+        $param,
+        $body = null
+    ) {
+        if (null !== $param) {
+            $this->guzzleOptions['query'] = $param;
+        }
+
+        if (null !== $body) {
+            $this->guzzleOptions['body'] = json_encode($body);
+        }
+
+        try {
+            $response = $this->guzzleClient->request(
+                $methode,
+                $uri,
+                $this->guzzleOptions
+            );
+
+            $json = $response->getBody();
+        } catch (RequestException $exception) {
+            $json = $exception->getResponse()->getBody()->getContents();
+        }
+
+        return json_decode($json);
+    }
+
+    private function buildHeader($token)
+    {
+        $header = [
+            'Authorization' => 'Bearer ' . $token,
+            'Accept'        => 'application/json',
         ];
-    }
 
-    public function domains($identifier = null)
-    {
-       return new Domains(new Core($this->config), $identifier);
-    }
-
-    public function articles($identifier = null)
-    {
-        return new Articles(new Core($this->config), $identifier);
+        $this->guzzleOptions = [
+            "headers" => $header
+        ];
     }
 }
--- test/test.php
+++ test/test.php
@@ -2,47 +2,13 @@
 
 require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
 
-use ApiClient\Client;
-use ApiClient\Model;
-
-$client = new Client(
-    'http://api-public.checkdomain.vm/v1/',
-    ''
+$client = new \ApiClient\Client(
+''
 );
 
-# Record anlegen
-$request = new Model\Request\Domain\Nameserver\Record();
-
-#$request->setName("321test")
-#    ->setType('A')
-#    ->setPriority(0)
-#    ->setTtl(180)
-#    ->setValue('192.168.0.1');
-
-# Record anlegen
-#$response = $client->domains(111)->nameservers()->records()->create($request);
-# Records ersetzen
-#$response = $client->domains(111)->nameservers()->records()->replace($request);
-# Record ersetzen
-#$response = $client->domains(111)->nameservers()->records(3)->replace($request);
-
-#Domains
-# Alle Domains gefiltert
-$filter = new Model\Request\Domain\DomainFilter();
-$filter
-    ->setLimit(1)
-    ->setPage(2);
-
-$response = $client->domains()->retrieve($filter);
-
-#$client->articles()->retrieve();
-
-
-# Eine Domain
-#$response = $client->domains(111)->retrieve();
-
-# Nameserver/Zone für eine Domain
-#$response = $client->domains(111)->nameservers()->retrieve();
-
+$response = $client->request('GET','articles', [
+    'limit' => 3,
+]);
 
 var_dump($response);
+
